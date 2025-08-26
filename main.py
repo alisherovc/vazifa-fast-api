078d4b56a3e067da7f5e1c27dbb4a67314173bb9
Alisherov <alisherovvv@proton.me>
2025-08-26 15:26:53 +0500

    ID boyicha chiqarish

--- main.py
+++ main.py
@@ -13,3 +13,10 @@ users = [
 @app.get("/users")
 def get_users():
     return users
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    for user in users:
+        if user["id"] == user_id:
+            return user
+    
